e6d03f3d5163869b32f03b0522c0b4a3831e64f5
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-10-04 12:23:37 +0300

    minor fix

--- src/controllers/actions.controller.ts
+++ src/controllers/actions.controller.ts
@@ -8,6 +8,8 @@ router.post("/actions", async (req: Request, res: Response, next: NextFunction) 
     const result = await actionsService.returnActionResponse(req.body);
     console.log('Action request:', req.body);
     console.log('Action response:', result);
+    // console.log('Action request:', inspect(req.body, { depth: null, colors: true }));
+    // console.log('Action response:', inspect(result, { depth: null, colors: true }));
     res.json(result);
   } catch (error) {
     next(error);
