dea3557c6bf2afd00227d663fc867a7a89e515b6
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-10-06 12:03:56 +0300

    minor fixes

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,6 @@ const server = express();
 server.use(express.json());
 server.use("/api", actionsController);
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on http://localhost:3000');
+server.listen(80, '0.0.0.0', () => {
+  console.log('Server is running on http://localhost:80');
 });
